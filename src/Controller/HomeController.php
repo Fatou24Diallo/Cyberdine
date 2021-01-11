db2dea8cffc2dda2188cd6062bef328f62294d6e
Fatou24Diallo <fatou2406@hotmail.fr>
2021-01-11 23:36:40 +0100

    Applications

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class HomeController extends AbstractController
 {
@@ -21,10 +26,36 @@ class HomeController extends AbstractController
     /**
      * @Route("/user", name="useracccount")
      */
-    public function userPage(): Response
+    public function userPage(Request $request): Response
     {
+
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+        $userName = $user->getUsername();
+        $userEmail = $user->getEmail();
+
         return $this->render('home/userpage.html.twig', [
             'controller_name' => 'HomeController',
+            'user_name' => $userName,
+            'user_email' => $userEmail,
+        ]);
+    }
+
+    /**
+     * @Route("/messagerie", name="messagerie")
+     */
+    public function messagerie(Request $request): Response
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+        $userName = $user->getUsername();
+        $userEmail = $user->getEmail();
+
+        return $this->render('home/messagerie.html.twig', [
+            'controller_name' => 'HomeController',
+            'user_name' => $userName,
+            'user_email' => $userEmail,
         ]);
     }
 }
